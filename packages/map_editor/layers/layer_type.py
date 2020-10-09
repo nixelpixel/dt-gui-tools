8b4033a9821405465be3f1f02bc5d0cba1aecab2
Dmitry Ivanov <darcenrall@gmail.com>
2020-10-09 16:22:26 +0300

    modify enum LayerType and upd relations

--- packages/map_editor/layers/layer_type.py
+++ packages/map_editor/layers/layer_type.py
@@ -1,13 +1,24 @@
 from enum import Enum
+from collections import namedtuple
+
+from classes.mapTile import MapTile
+from classes.mapObjects import MapBaseObject, SignObject, GroundAprilTagObject, WatchTowerObject, RegionObject, ActorObject, DecorationObject
+
+LayerInfo = namedtuple('LayerInfo', ['type', 'obj_type', 'obj_class'])
 
 
 class LayerType(Enum):
-    TILES = '0-tiles'
-    TRAFFIC_SIGNS = '1-signs'
-    GROUND_APRILTAG = '2-groundtags'
-    WATCHTOWERS = '3-watchtowers'
-    REGIONS = '4-regions'
-    ITEMS = 'objects'
+    TILES = LayerInfo('0-tiles', None, MapTile)
+    TRAFFIC_SIGNS = LayerInfo('1-signs', 'sign', SignObject)
+    GROUND_APRILTAG = LayerInfo('2-groundtags', 'apriltag', GroundAprilTagObject)
+    WATCHTOWERS = LayerInfo('3-watchtowers', 'watchtower', WatchTowerObject)
+    REGIONS = LayerInfo('4-regions', None, RegionObject)
+    ACTORS = LayerInfo('5-actors', 'actor', ActorObject)
+    DECORATIONS = LayerInfo('6-decorations', 'decoration', DecorationObject)
+    ITEMS = LayerInfo('objects', 'object', MapBaseObject)
 
     def __str__(self):
-        return self.value
+        return self.value.type
+
+    def get_obj_class(self):
+        return self.value.obj_class
--- packages/map_editor/layers/relations.py
+++ packages/map_editor/layers/relations.py
@@ -1,31 +1,8 @@
 from layers.layer_type import LayerType
-from classes.mapObjects import MapBaseObject, SignObject, CityObject, WatchTowerObject, GroundAprilTagObject
+#from classes.mapObjects import MapBaseObject, SignObject, CityObject, WatchTowerObject, GroundAprilTagObject
 
 
-LAYER_TYPE_WITH_OBJECTS = (LayerType.TRAFFIC_SIGNS, LayerType.GROUND_APRILTAG, LayerType.WATCHTOWERS, LayerType.ITEMS)
-
-LAYER_OBJECTS = {
-    LayerType.TRAFFIC_SIGNS: {
-        'type': 'sign',
-        'class': SignObject
-    },
-    LayerType.GROUND_APRILTAG: {
-        'type': 'apriltag',
-        'class': GroundAprilTagObject
-    },
-    LayerType.WATCHTOWERS: {
-        'type': 'watchtower',
-        'class': WatchTowerObject
-    },
-    LayerType.ITEMS: {
-        'type': 'objects',
-        'class': CityObject
-    },
-    'default': {
-        'type': '',
-        'class': MapBaseObject
-    }
-}
+LAYER_TYPE_WITH_OBJECTS = (LayerType.TRAFFIC_SIGNS, LayerType.GROUND_APRILTAG, LayerType.WATCHTOWERS, LayerType.ACTORS, LayerType.DECORATIONS)
 
 
 def get_class_by_object_type(object_type):
@@ -35,23 +12,20 @@ def get_class_by_object_type(object_type):
     :param object_type: type of object
     :return: class of map object
     """
-    for layer_type, info in LAYER_OBJECTS.items():
-        if object_type == info['type']:
-            return info['class']
-    return LAYER_OBJECTS['default']['class']
+    for layer_type_info in list(LayerType):
+        layer_type, obj_type, obj_class = layer_type_info.value
+        if object_type == obj_type:
+            return obj_class
+    return LayerType.ITEMS.get_obj_class()
 
 
 def get_class_by_layer_type(layer_type):
     """
     Get map object class from classes.mapObjects for layer_type
-    If layer_type doesn't exist, return MapBaseObject
     :param layer_type: LayerType
     :return: class of map object
     """
-    if layer_type in LAYER_OBJECTS:
-        return LAYER_OBJECTS[layer_type]['class']
-    else:
-        return LAYER_OBJECTS['default']['class']
+    return layer_type.get_obj_class()
 
 
 def get_layer_type_by_object_type(object_type):
@@ -61,11 +35,11 @@ def get_layer_type_by_object_type(object_type):
     :param object_type: type of object
     :return: LayerType
     """
-    for layer_type, info in LAYER_OBJECTS.items():
-        if object_type == info['type']:
-            return layer_type
-    else:
-        return LayerType.ITEMS
+    for layer_type_info in list(LayerType):
+        layer_type, obj_type, obj_class = layer_type_info.value
+        if object_type == obj_type:
+            return layer_type_info
+    return LayerType.ITEMS
 
 
 def get_layer_type_by_value(type_value):
