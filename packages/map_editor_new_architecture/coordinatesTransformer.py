0fd91c528807bae0986fd79ba9d704782e9d7ac6
Valentina-Gol <vip.valya.golubeva@mail.ru>
2022-06-22 13:39:14 +0300

    added tests for coordinates transformer

--- packages/map_editor_new_architecture/coordinatesTransformer.py
+++ packages/map_editor_new_architecture/coordinatesTransformer.py
@@ -1,15 +1,20 @@
 class CoordinatesTransformer:
     """coordinate of mouse, pressevent, moveevent, offset,"""
     def __init__(self, scale: float, size_map: int, grid_size: float):
-        self.scale = scale
-        self.size_map = size_map
-        self.grid_size = grid_size
+        self.scale: float = scale
+        self.size_map: int = size_map
+        self.grid_size: float = grid_size
 
-    def get_x_from_view(self, x_view: float, offset_x: float = 0.0, obj_width: float = 0.0) -> float:
+    def get_x_from_view(self, x_view: float,
+                        offset_x: float = 0.0,
+                        obj_width: float = 0.0) -> float:
         return (x_view - obj_width + offset_x) / self.scale / self.grid_size
 
-    def get_y_from_view(self, y_view: float, offset_y: float = 0.0, obj_height: float = 0.0) -> float:
-        return self.size_map - (y_view + obj_height + offset_y) / self.scale / self.grid_size
+    def get_y_from_view(self, y_view: float,
+                        offset_y: float = 0.0,
+                        obj_height: float = 0.0) -> float:
+        return self.size_map - (y_view + obj_height + offset_y) \
+               / self.scale / self.grid_size
 
     def get_x_to_view(self, x_real: float) -> float:
         return x_real * self.scale * (self.grid_size + 1)
---
+++ packages/map_editor_new_architecture/tests/testCoordinateTransformer/test_coord_transf.py
@@ -0,0 +1,66 @@
+import unittest
+import json
+from coordinatesTransformer import CoordinatesTransformer
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_x_to_view(self):
+        with open("./tests/testCoordinateTransformer/test_x_to_view.json",
+                  "r") as file:
+            self.test_cases = json.load(file)
+        for test_name in self.test_cases:
+            with self.subTest(test_name=test_name):
+                case = self.test_cases[test_name]
+                coord_trans = CoordinatesTransformer(case['scale'],
+                                                     case['size_map'],
+                                                     case['grid_size'])
+                ret = coord_trans.get_x_to_view(case['x'])
+                self.assertEqual(ret, case['ret'])
+
+    def test_y_to_view(self):
+        with open("./tests/testCoordinateTransformer/test_y_to_view.json",
+                  "r") as file:
+            self.test_cases = json.load(file)
+        for test_name in self.test_cases:
+            with self.subTest(test_name=test_name):
+                case = self.test_cases[test_name]
+                coord_trans = CoordinatesTransformer(case['scale'],
+                                                     case['size_map'],
+                                                     case['grid_size'])
+                ret = coord_trans.get_x_to_view(case['y'])
+                self.assertEqual(ret, case['ret'])
+
+    def test_x_from_view(self):
+        with open("./tests/testCoordinateTransformer/test_x_from_view.json",
+                  "r") as file:
+            self.test_cases = json.load(file)
+        for test_name in self.test_cases:
+            with self.subTest(test_name=test_name):
+                case = self.test_cases[test_name]
+                coord_trans = CoordinatesTransformer(case['scale'],
+                                                     case['size_map'],
+                                                     case['grid_size'])
+                ret = coord_trans.get_x_from_view(case['x'],
+                                                  case['offset_x'],
+                                                  case['obj_width'])
+                self.assertLess(abs((ret - case['ret'])), 0.001)
+
+    def test_y_from_view(self):
+        with open("./tests/testCoordinateTransformer/test_y_from_view.json",
+                  "r") as file:
+            self.test_cases = json.load(file)
+        for test_name in self.test_cases:
+            with self.subTest(test_name=test_name):
+                case = self.test_cases[test_name]
+                coord_trans = CoordinatesTransformer(case['scale'],
+                                                     case['size_map'],
+                                                     case['grid_size'])
+                ret = coord_trans.get_y_from_view(case['y'],
+                                                  case['offset_y'],
+                                                  case['obj_height'])
+                self.assertLess(abs((ret - case['ret'])), 0.001)
+
+
+if __name__ == '__main__':
+    unittest.main()
